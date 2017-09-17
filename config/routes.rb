5d14ff22c174a98f44712c367ebd7dbe2878318b
Anthony Nguyen <neoterix@gmail.com>
2017-09-17 11:31:45 -0400

    Created home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+    
+    root "pages#home"
+    get "about" => "pages#about" # create about_path
+    
+    
+  # Old Route  
+  # get 'pages/home'  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+k 
